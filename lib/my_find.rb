13084480363df625248f1369a1fd9b0cafaa12ca
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-21 02:57:28 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,11 @@
 
 
 def my_find(collection)
-  i = 0 
-  while i < collection.length 
-  yield(collection[i])
-  i = i + 1 
-end
-
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
 end
